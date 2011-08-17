1a158db5f2baf294a52a0a7dbec8c0a8fa963805
AmaiSaeta <amaisaeta@gmail.com>
2011-08-17 11:51:35 +0900

    チェック/未処理数のカウンタを追加

--- bookmeter-multi-add.user.js
+++ bookmeter-multi-add.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           bookmeter-multi-add
-// @version        0.9.2.20110811
+// @version        0.9.3.20110817
 // @namespace      http://amaisaeta.seesaa.net/
 // @description    読書メーターの検索画面にて、複数書籍の一括登録の為のUIを用意します。
 // @license        MIT License; http://www.opensource.org/licenses/mit-license.php
@@ -17,8 +17,17 @@
 	const dateSMName = 'read_date_d';
 
 	const checkboxClassName = prefix + 'asin';
-	const candidatesBlock = document.getElementById('main_left').getElementsByClassName('inner')[0];
-	const results = document.evaluate('*[contains(concat(" ", normalize-space(@class), " "), " book ")]', candidatesBlock, null, 7, null);
+	const results = document.getElementById('main_left').getElementsByClassName('book');
+
+	var remainCounterElem;
+	function updateRemainCounter(num) {
+		LOG('in updateRemainConter(' + num + ')');
+		remainCounterElem.nodeValue = num;
+	}
+	function getRemainNum() {
+		LOG('in getRemainNum()');
+		return parseInt(remainCounterElem.nodeValue);
+	}
 
 	function registerBook(asin, date, category) {
 		LOG('in registerBook('+asin+',('
@@ -59,8 +68,15 @@
 				xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded;charset=UTF-8");
 				LOG('edit: url: ' + url + '\nquery: ' + query);
 				xhr.onreadystatechange = function() {
-					if(xhr.readyState == 4/*complete*/ && xhr.status == 200) {
-						LOG('edit query="'+query+'" completed!');
+					const remainNum = getRemainNum();
+					if(xhr.readyState == 4/*complete*/) {
+						if(xhr.status == 200) {
+							LOG('edit query="'+query+'" completed!');
+						} else {
+							LOG('edit query="'+query+'" failed!');
+						}
+						updateRemainCounter(remainNum - 1);
+						LOG("remain: " + remainNum - 1);
 					}
 				}
 				xhr.send(query);
@@ -91,6 +107,8 @@
 	function registerBooks(event) {
 		LOG("in registerBooks()");
 
+		var checks = getCheckElems(results);
+		remainNum = checks.length;
 		var date = null;
 
 		LOG("fumei is... " + document.getElementById(prefix + 'fumei').checked);
@@ -114,21 +132,46 @@
 
 		LOG('category: ' + category);
 
-		LOG('item num: ' + results.snapshotLength);
-		for(var i = 0; i < results.snapshotLength; ++i) {
-			var item = results.snapshotItem(i).getElementsByTagName('input')[0];
-			if(item.checked) {
-				LOG('find checked item[asin=' + item.value + ']!');
-				if(!registerBook(item.value, date, category)) {
-					LOG('failed: ' + item.value);
-				}
-				LOG('remain: ' + (results.snapshotLength - i) + ' / ' + results.snapshotLength);
+		LOG('item num: ' + checks.length);
+		for(var i = 0; i < checks.length; ++i) {
+			var item = checks[i].getElementsByTagName('input')[0];
+			if(!registerBook(item.value, date, category)) {
+				LOG('failed: ' + item.value);
 			}
+			LOG('remain: ' + (checks.length - i) + ' / ' + checks.length);
 		}
+
+		// 全完了時通知
+		var completeCheckerTID = setInterval(function() {
+			if(remainNum <= 0) {
+				clearInterval(completeCheckerTID);
+				alert('complate!');
+			}
+		}, 100);
+
 		event.preventDefault();
+
+		// co-functions {{{
+		function getCheckElems(elems) {
+			LOG('in getCheckElems(' + (typeof elems) + (typeof elems === 'Array' ? '['+elem.length+']':'') + ')');
+
+			var res = [];
+			for(var i = 0; i < elems.length; ++i) {
+				// [TODO] Autopagerizeで読み込んだ部分にはcheckboxが付与されていないので、そのチェックを行っている。
+				// [TODO] Autopagerize対応は次更新時に行う予定である
+				var e = elems[i].getElementsByClassName(checkboxClassName);
+				if((e.length != 0) && e[0].checked) {
+					res.push(elems[i]);
+				}
+			}
+			return res;
+		}
+		// }}}
 	}
 
 	function addSubmitForm() { // {{{
+		LOG('in addSubmitForm()');
+
 		const now = new Date();
 
 		var readY = createSelect(yearSMName, 10, function(i) {
@@ -168,12 +211,27 @@
 
 		fs = document.createElement('fieldset');
 		fs.appendChild(submit);
+		fs.appendChild((function() {
+			var parent = document.createElement('dl');
+			var head = parent.appendChild(document.createElement('dt'));
+			var body = parent.appendChild(document.createElement('dd'));
+			head.appendChild(document.createTextNode('残り'));
+			remainCounterElem = body.appendChild(document.createTextNode('0'));
+			parent.style.display  = head.style.display = body.style.display = 'inline';
+			parent.style.marginLeft = '1em';
+			head.style.paddingRight = '1em';
+
+			return parent;
+		})());
 		form.appendChild(fs);
 
 		var block = document.createElement('div');
 		block.className = 'book_edit_area';
 		block.appendChild(form);
-		candidatesBlock.parentNode.insertBefore(block, candidatesBlock);
+
+		var pos = document.getElementById('main_left').getElementsByClassName('inner')[0];
+		pos.parentNode.insertBefore(block, pos);
+		//insertAfter(pos.parentNode, block, pos);
 
 		// co-functions {{{
 		function createSelect(name, num, func) {
@@ -201,14 +259,22 @@
 	}	// }}}
 
 	function addCheckboxies() {	// {{{
-		for(var i = 0; i < results.snapshotLength; ++i) {
-			var ssItem = results.snapshotItem(i);
+		LOG('in addCheckboxies()');
+
+		for(var i = 0; i < results.length; ++i) {
 			var checkbox = document.createElement('input');
 			checkbox.type = 'checkbox';
 			checkbox.className = checkboxClassName;
 			checkbox.name = checkboxClassName;
-			checkbox.value = ssItem.getElementsByTagName('a')[0].href.match(/[0-9a-zA-Z]+$/);
-			ssItem.appendChild(checkbox);
+			checkbox.value = results[i].getElementsByTagName('a')[0].href.match(/[0-9a-zA-Z]+$/);
+			checkbox.addEventListener('click', function(event) {
+				LOG('in checkbox.onclick handler');
+				const remainNum = getRemainNum();
+				var m = event.target.checked ? 1 : -1;
+				updateRemainCounter(remainNum + m);
+				enableSubmitButton(remainNum > 0);
+			}, false);
+			results[i].appendChild(checkbox);
 
 		}
 	}	// }}}
@@ -216,6 +282,11 @@
 	function str_w(src, w) { // {{{
 		const needW = w - src.toString().length;
 		return (needW <= 0 ? '' : (new Array(needW + 1)).join('0')) + src;
+	}	// }}}
+
+ 	// via: http://www.dustindiaz.com/top-ten-javascript/
+	function insertAfter(parent, node, referenceNode) { // {{{
+		parent.insertBefore(node, referenceNode.nextSibling);
 	}	// }}}
 
 	addCheckboxies();
